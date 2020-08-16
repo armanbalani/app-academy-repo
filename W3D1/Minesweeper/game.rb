3f38b2c89563cf0ff3974cbf6c126ad0c4a105f9
Arman Balani <arubalani@gmail.com>
2020-08-16 13:16:24 +0530

    implemented save/load functionality

--- W3D1/Minesweeper/game.rb
+++ W3D1/Minesweeper/game.rb
@@ -1,5 +1,6 @@
 require_relative("./board.rb")
 require_relative("./tile.rb")
+require 'yaml'
 
 class Game
     attr_accessor :game_board
@@ -17,13 +18,18 @@ class Game
 
     def play_turn
         @game_board.display_grid
-        puts "Enter the position you would like to reveal. Or if you'd like to flag a position type the word 'flag'"
+        puts "Enter the position you would like to reveal.\nOr\nType 'flag' to flag a position you think might be a bomb.\nType 'save' to save your current session.\nType 'load' to load a previous session"
         input = gets.chomp
         if input == "flag"
             puts "Enter the position you would like to flag"
             flag_input = gets.chomp
             tile_to_flag = get_tile_at_pos(flag_input)
             tile_to_flag.flag
+        elsif input == "save"
+            self.save
+            puts "Game saved successfully"
+        elsif input == "load"
+            self.load
         else 
             tile_at_pos = get_tile_at_pos(input)
             if !tile_at_pos.status
@@ -33,7 +39,7 @@ class Game
                 @game_board.display_grid
                 puts "Game over!"
             end   
-        end    
+        end
 
     end
 
@@ -45,6 +51,18 @@ class Game
     def game_over?
         @game_board.grid.any? do |row|
             row.any? { |tile| tile.status && tile.face }
+        end
+    end
+
+    def save
+        File.open("save_data.yml", "w") do |f|     
+            f.write(@game_board.to_yaml)   
+        end
+    end
+
+    def load
+        if File.exist?("save_data.yml")
+            @game_board = YAML.load_file("save_data.yml")
         end
     end
 
