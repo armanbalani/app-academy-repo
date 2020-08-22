2bd13f60b38dc0d87c991ab2a9df6230fdd2ed58
Arman Balani <arubalani@gmail.com>
2020-08-22 20:45:29 +0530

    finally finished make_move logic

--- W3D4/mancala/skeleton/lib/board.rb
+++ W3D4/mancala/skeleton/lib/board.rb
@@ -1,4 +1,5 @@
 require_relative 'player'
+require 'byebug'
 
 
 class Board
@@ -33,21 +34,48 @@ class Board
   def make_move(start_pos, current_player_name)
     stone_count = @cups[start_pos].length
     @cups[start_pos].clear
-    players = Player.all
-    ((start_pos + 1)..(start_pos + stone_count)).each do |i|
-      if current_player_name.side == 1
-        @cups[i] << :stone unless i == 13
-      elsif current_player_name.side == 2
-        @cups[i] << :stone unless i == 6
+    if start_pos == 12
+      next_pos = 0
+    else
+      next_pos = start_pos + 1
+    end
+    end_pos = (next_pos + (stone_count - 1))
+    (next_pos..end_pos).each do |i|
+      if (0..5).include?(start_pos)
+        @cups[i % 12] << :stone
+      else
+        if i == 6
+          @cups[7] << :stone
+        end
+        @cups[i % 14] << :stone
       end
+    end
+    # render
+    next_turn(end_pos, start_pos)
+
+  end
+
+  def next_turn(ending_cup_idx, start_pos)
+    # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
+    if (0..5).include?(start_pos)
+      side = 1
+    else
+      side = 2
+    end
+
+    end_index = ending_cup_idx % 14
+
+    if @cups[end_index].length == 1 && end_index != 13 && end_index != 6
+      return :switch
+    elsif side == 1 && end_index == 6
+      :prompt
+    elsif side == 2 && end_index == 13
+      :prompt
+    else
+      end_index
     end
 
 
-    
-  end
-
-  def next_turn(ending_cup_idx)
-    # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
   end
 
   def render
@@ -64,3 +92,8 @@ class Board
   def winner
   end
 end
+
+# board = Board.new("Erica", "James")
+# # board.cups[0].clear
+# debugger
+# p board.make_move(9, "James")
