bea38f935f1b11e2ef6a82ea116a86bec5b8bb96
Arman Balani <arubalani@gmail.com>
2020-08-24 14:08:00 +0530

    completed specs

--- W4D1/simon/skeleton/lib/simon.rb
+++ W4D1/simon/skeleton/lib/simon.rb
@@ -4,19 +4,31 @@ class Simon
   attr_accessor :sequence_length, :game_over, :seq
 
   def initialize
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 
   def play
+    until game_over
+      take_turn
+    end
+    game_over_message
+    reset_game if game_over
 
   end
 
   def take_turn
-
+    show_sequence
+    require_sequence
+    if !game_over
+      round_success_message
+      @sequence_length += 1
+    end
   end
 
   def show_sequence
-
+    add_random_color
   end
 
   def require_sequence
@@ -24,7 +36,9 @@ class Simon
   end
 
   def add_random_color
-
+    random_color = COLORS.sample
+    @seq << random_color
+    @seq
   end
 
   def round_success_message
@@ -36,6 +50,9 @@ class Simon
   end
 
   def reset_game
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
+
 end
