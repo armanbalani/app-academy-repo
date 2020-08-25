d4a20d920b5cf68257d473995ea66067b5ddbb60
Arman Balani <arubalani@gmail.com>
2020-08-25 15:51:49 +0530

    implemented #eat tests

--- W4D2/rspec-hw/skeleton/spec/dessert_spec.rb
+++ W4D2/rspec-hw/skeleton/spec/dessert_spec.rb
@@ -49,9 +49,14 @@ describe Dessert do
   end
 
   describe "#eat" do
-    it "subtracts an amount from the quantity"
+    it "subtracts an amount from the quantity" do
+      dessert.eat(1) 
+      expect(dessert.quantity).to eq(11)
+    end
 
-    it "raises an error if the amount is greater than the quantity"
+    it "raises an error if the amount is greater than the quantity" do
+      expect { dessert.eat(13) }.to raise_error("not enough left!")
+    end
   end
 
   describe "#serve" do
