b53dfa7bf27a7922455c255fd2692501d2ef8fc8
Arman Balani <arubalani@gmail.com>
2020-08-28 19:44:38 +0530

    completed project, still struggling with space and time complexity. REVISIT

--- W4D4/execution-time-differences.rb
+++ W4D4/execution-time-differences.rb
@@ -30,9 +30,28 @@
 #     largest_contiguous_subsum(list) # => 8 (from [7, -6, 7])
 
 def largest_contiguous_subsum(array)
-    
+    sub_arrays = []
+    (0...array.length).each do |i|
+        (0...array.length).each do |x|
+            sub_arrays << array[i..x] unless array[i..x].empty?
+        end
+    end
+    p sub_arrays.length
+    max_array = sub_arrays.max_by { |sub| sub.sum }
+    max_array.sum
+
+end
+
+# time complexity = O(n^2 + x)
+
+list = [2, 3, -6, 7, -6, 7, 3, 4, 5, 10, 7]
+
+def better_largest_contiguous_subsum(array)
+    largest_sum = 0
+    (0...array.length).each do |i|
+        x = i
+        until x == array.length
+    end
 end
 
 
-list = [2, 3, -6, 7, -6, 7]
-p largest_contiguous_subsum(list)
