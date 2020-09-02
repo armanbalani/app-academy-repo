bbbcf04e10d5d9cac84073cb9e5528e9717f6574
Arman Balani <arubalani@gmail.com>
2020-09-02 13:50:52 +0530

    finished phase 2 of project

--- W5D1/hashmap-caching-project/lib/p02_hashing.rb
+++ W5D1/hashmap-caching-project/lib/p02_hashing.rb
@@ -4,11 +4,24 @@ end
 
 class Array
   def hash
+    initial = self.length
+    (0...self.length).each do |i|
+      initial += (self[i] + i.hash)/self[i]
+    end
+    initial
   end
 end
 
 class String
   def hash
+    alhapbet = ("a".."z").to_a
+    initial = 0
+    self.each_char do |char|
+      index = alhapbet.find_index(char)
+      initial += index.hash/(self.length + index.to_i)
+      initial /= index.to_i + 1
+    end
+    initial 
   end
 end
 
@@ -16,6 +29,11 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    initial = 0
+    self.each do |k, v|
+      initial += (k.hash + v.hash)
+    end
+    initial
   end
+  
 end
