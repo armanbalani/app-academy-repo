b8809b4ba034da645ba6e4a791f65147b52788e4
Arman Balani <arubalani@gmail.com>
2020-09-02 14:01:24 +0530

    completed phase 3 of project

--- W5D1/hashmap-caching-project/lib/p03_hash_set.rb
+++ W5D1/hashmap-caching-project/lib/p03_hash_set.rb
@@ -1,23 +1,37 @@
 class HashSet
   attr_reader :count
 
-  def initialize(num_buckets = 8)
+  def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
     @count = 0
   end
 
   def insert(key)
-  end
-
-  def include?(key)
+    if @count >= num_buckets
+      resize!
+    end
+    unless self.include?(key)
+      self[key.hash] << key 
+      @count += 1
+    end
   end
 
   def remove(key)
+    if self.include?(key)
+      self[key.hash].delete(key)
+      @count -= 1
+    end
+  end
+
+  def include?(key)
+    self[key.hash % num_buckets].include?(key)
+    
   end
 
   private
 
   def [](num)
+    @store[num % num_buckets]
     # optional but useful; return the bucket corresponding to `num`
   end
 
@@ -26,5 +40,15 @@ class HashSet
   end
 
   def resize!
+    num_buckets.times do
+      @store << Array.new
+    end
+    @store.each do |bucket|
+      bucket.each do |num|
+        bucket.delete(num)
+        self.insert(num)
+        @count -= 1
+      end
+    end
   end
 end
