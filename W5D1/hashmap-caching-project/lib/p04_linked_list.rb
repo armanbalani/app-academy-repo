3e0faf577d6314d60e9c6f1371f52dbae8f3614b
Arman Balani <arubalani@gmail.com>
2020-09-03 15:46:46 +0530

    finished phase 4 of project

--- W5D1/hashmap-caching-project/lib/p04_linked_list.rb
+++ W5D1/hashmap-caching-project/lib/p04_linked_list.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 class Node
   attr_reader :key
   attr_accessor :val, :next, :prev
@@ -17,10 +19,24 @@ class Node
     # optional but useful, connects previous link to next link
     # and removes self from list.
   end
+
+  def self.all
+    ObjectSpace.each_object(self).to_a
+  end
+
+
 end
 
 class LinkedList
+  include Enumerable
+
+  attr_reader :head, :tail
+
   def initialize
+    @head = Node.new
+    @tail = Node.new
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -29,30 +45,57 @@ class LinkedList
   end
 
   def first
+    @head.next
   end
 
   def last
+    @tail.prev
   end
 
   def empty?
+    @head.next == @tail && @tail.prev == @head
+  end
+
+  def get_node(key)
+    each { |link| return link if link.key == key }
+    nil
   end
 
   def get(key)
+    each { |link| return link.val if link.key == key }
+    nil
   end
 
   def include?(key)
+    !get_node(key).nil?
   end
 
   def append(key, val)
+    node = Node.new(key, val)
+    @tail.prev.next = node
+    node.next = @tail
+    node.prev = @tail.prev
+    @tail.prev = node
   end
 
   def update(key, val)
+    node = get_node(key)
+    node.val = val unless node.nil?
   end
 
   def remove(key)
+    node = get_node(key)
+    node.prev.next = node.next
+    node.next.prev = node.prev
   end
 
-  def each
+  def each(&prc)
+    current_node = @head.next
+    until current_node == @tail
+      prc.call(current_node)
+      current_node = current_node.next
+    end
+    
   end
 
   # uncomment when you have `each` working and `Enumerable` included
@@ -60,3 +103,4 @@ class LinkedList
   #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
   # end
 end
+
