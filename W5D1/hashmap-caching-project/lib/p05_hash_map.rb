536134bb93c34fb5b1d42aa1e485f2e585ca3c9d
Arman Balani <arubalani@gmail.com>
2020-09-03 16:38:08 +0530

    finished phase 5 of project

--- W5D1/hashmap-caching-project/lib/p05_hash_map.rb
+++ W5D1/hashmap-caching-project/lib/p05_hash_map.rb
@@ -1,6 +1,7 @@
 require_relative 'p04_linked_list'
 
 class HashMap
+  include Enumerable
   attr_accessor :count
 
   def initialize(num_buckets = 8)
@@ -9,18 +10,36 @@ class HashMap
   end
 
   def include?(key)
+    bucket(key).include?(key)
   end
 
   def set(key, val)
+    bucket = bucket(key)
+    resize! if @count >= num_buckets
+    if bucket.include?(key)
+      bucket.update(key, val)
+    else
+      bucket.append(key, val)
+      @count += 1
+    end
   end
 
   def get(key)
+    bucket = bucket(key)
+    bucket.get(key)
   end
 
   def delete(key)
+    bucket(key).remove(key)
+    @count -= 1
   end
 
-  def each
+  def each(&prc)
+    @store.each do |bucket|
+      bucket.each do |node|
+        prc.call(node.key, node.val)
+      end
+    end
   end
 
   # uncomment when you have Enumerable included
@@ -41,9 +60,32 @@ class HashMap
   end
 
   def resize!
+    nodes = []
+    @store.each do |bucket|
+      bucket.each do |node|
+        unless node == bucket.head || node == bucket.tail
+          nodes << node
+          self.delete(node.key)
+        end
+      end
+    end
+    num_buckets.times do
+      @store << LinkedList.new
+    end
+    nodes.each do |node|
+      self.set(node.key, node.val)
+    end
   end
 
   def bucket(key)
+    bucket_index = key.hash % num_buckets  
+    @store[bucket_index]  
     # optional but useful; return the bucket corresponding to `key`
   end
 end
+
+    hash = HashMap.new
+    hash.set(:first, 1)
+    hash.set(:second, 2)
+    hash.set(:third, 3)
+    hash
