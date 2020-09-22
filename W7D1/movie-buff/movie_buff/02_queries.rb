ebfaabb2aacec1b8da45bb17ff8af76c0c316549
Arman Balani <arubalani@gmail.com>
2020-09-22 19:45:34 +0530

    finished movie buff

--- W7D1/movie-buff/movie_buff/02_queries.rb
+++ W7D1/movie-buff/movie_buff/02_queries.rb
@@ -23,11 +23,13 @@ def vanity_projects
   # Show the movie id and title and director's name.
 
   # Note: Directors appear in the 'actors' table.
+  Movie.select("movies.id, title, name").joins(:actors).where("director_id = castings.actor_id AND castings.ord = 1")
 
 end
 
 def most_supportive
   # Find the two actors with the largest number of non-starring roles.
   # Show each actor's id, name and number of supporting roles.
+  Casting.select("actors.id, actors.name, COUNT(*) AS roles").joins(:actor).where("castings.ord != 1").group("actors.id, actors.name").order("roles DESC").limit(2)
 
 end
