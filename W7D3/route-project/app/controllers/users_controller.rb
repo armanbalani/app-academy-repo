30764f878a4b9c693aade8210b2ceedc950884f4
Arman Balani <arubalani@gmail.com>
2020-09-25 16:14:44 +0530

    finished most of project

--- W7D3/route-project/app/controllers/users_controller.rb
+++ W7D3/route-project/app/controllers/users_controller.rb
@@ -1,5 +1,18 @@
 class UsersController < ApplicationController
     def index
-        render plain: "I'm in the index action!"
+        render json: User.all
+    end
+
+    def create
+        user = User.new(params.require(:user).permit(:name, :email))
+        if user.save
+            render json: user
+        else
+            render json: user.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
+    def show
+        render json: User.find(params[:id])
     end
 end
