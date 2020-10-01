99cf99aba9825d36981d9102c53e8a8c70dc6b5e
Arman Balani <arubalani@gmail.com>
2020-10-01 13:15:56 +0530

    artwork share actions working with routes

--- W7D3/route-project/config/routes.rb
+++ W7D3/route-project/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :show, :create, :update, :destroy]
   resources :artworks
+  resources :artwork_shares, only: [:create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
