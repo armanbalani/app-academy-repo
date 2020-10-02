7fd1baae01716da9cfd419d44402d4c8f2c43c99
Arman Balani <arubalani@gmail.com>
2020-10-02 14:21:59 +0530

    finished comments functionality

--- W8D1/art-share-api-part-2/config/routes.rb
+++ W8D1/art-share-api-part-2/config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :show, :create, :update, :destroy] do
     resources :artworks, only: [:index]
+    resources :comments, only: [:index]
   end
-  resources :artworks, only: [:show, :create, :update, :destroy]
+  resources :artworks, only: [:show, :create, :update, :destroy] do
+    resources :comments, only: [:index]
+  end
+  resources :comments, only: [:create, :destroy]
   resources :artwork_shares, only: [:create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
