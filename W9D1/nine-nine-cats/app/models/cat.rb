53bd40ee07109df1021e330bfac4ca51b7581a31
Arman Balani <arubalani@gmail.com>
2020-10-14 15:42:59 +0530

    finished cat model

---
+++ W9D1/nine-nine-cats/app/models/cat.rb
@@ -0,0 +1,21 @@
+require 'action_view'
+
+class Cat < ApplicationRecord
+    include ActionView::Helpers::DateHelper
+
+    CATS = ["white", "black", "orange", "brown", "grey"]
+    
+    validates :name, presence: true
+    validates :color, presence: true
+    validates :sex, presence: true
+    validates :birth_date, presence: true
+    validates :description, presence: true
+
+    validates :color, inclusion: { in: CATS, message: "not a valid color" }
+    validates :sex, inclusion: { in: %w(M F), message: "not a valid sex" }
+
+
+    def age
+        time_ago_in_words(self.birth_date) + " old"
+    end
+end
