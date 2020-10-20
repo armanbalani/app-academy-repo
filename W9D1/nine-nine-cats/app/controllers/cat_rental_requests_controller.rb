3572b1a27aeb9523c1e7a75fed64b336036e3f08
Arman Balani <arubalani@gmail.com>
2020-10-20 15:20:09 +0530

    finished form for cat rental requests

--- W9D1/nine-nine-cats/app/controllers/cat_rental_requests_controller.rb
+++ W9D1/nine-nine-cats/app/controllers/cat_rental_requests_controller.rb
@@ -1,7 +1,22 @@
 class CatRentalRequestsController < ApplicationController
   def new
+    render :new
   end
 
   def create
+    
+    cat_rental_request = CatRentalRequest.new(cat_rental_request_params)
+    if cat_rental_request.save
+      redirect_to cat_url(cat_rental_request.cat_id)
+    else
+      render :new
+    end
+
+  end
+
+  private
+
+  def cat_rental_request_params
+    params.require(:cat_rental_request).permit(:start_date, :end_date, :cat_id)
   end
 end
